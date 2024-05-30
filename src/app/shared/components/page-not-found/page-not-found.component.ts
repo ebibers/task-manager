2297914e481a69cf9e1716722aaa4e47b3a7306b
ebibers <bibersemils@gmail.com>
2024-05-30 15:01:02 +0300

    implemented design and go back functionality for 404 page

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'page-not-found',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
 export class PageNotFoundComponent {
+  constructor(private location : Location) {}
+
+  goBack() {
+    this.location.back();
+  }
 }
