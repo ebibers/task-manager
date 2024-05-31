cdc14e312ea7f344d67805bafce7acec5ee4570d
ebibers <bibersemils@gmail.com>
2024-06-01 01:45:08 +0300

    adjusted 404 page design for mobile viewport

--- src/app/task/shared/components/task-nav-bar/task-nav-bar.component.ts
+++ src/app/task/shared/components/task-nav-bar/task-nav-bar.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'task-nav-bar',
   standalone: true,
-  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
   templateUrl: './task-nav-bar.component.html',
   styleUrl: './task-nav-bar.component.scss'
 })
