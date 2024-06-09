8c97fde3d2479c3b919f64f3c3609cd5377864b0
ebibers <bibersemils@gmail.com>
2024-06-10 02:11:04 +0300

    basic layout for task editing

--- src/app/task/task-details/task-details.component.ts
+++ src/app/task/task-details/task-details.component.ts
@@ -7,17 +7,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'task-details',
   standalone: true,
-  imports: [DatePipe, MatIconModule],
+  imports: [DatePipe, MatIconModule, MatButtonModule, MatTooltipModule, MatSlideToggleModule, MatInputModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.scss'
 })
 export class TaskDetailsComponent implements OnInit, OnDestroy {
   private destroy$: Subject<void> = new Subject<void>();
   task : Task | null = null;
+  editable: boolean = false;
 
   constructor(private taskService: TaskService, private router : Router, private route: ActivatedRoute) {}
 
@@ -39,6 +44,16 @@ export class TaskDetailsComponent implements OnInit, OnDestroy {
         });
       }
     });
+  }
+
+  onEdit() {
+    this.editable = !this.editable;
+  }
+
+  onSave() {
+
+
+    this.editable = !this.editable;
   }
 
   ngOnDestroy(): void {
